f11b30fccd9f00ded5b8e599fe8c20489b00214e
Denis Molloy <denis.molloy@gmail.com>
2018-03-02 01:08:03 -0800

    more styling

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,19 @@
-var h1Text = "SHALL WE PLAY A GAME?";
-function printSentence(id, sentence, speed){
-    var index=0;
-    var intObject= setInterval(function() {
-    document.getElementById(id).innerHTML+=sentence[index];
-    index++;
-    if(index==sentence.length){
-    clearInterval(intObject);
-    }
-    }, speed);
-}
-var mp3 = document.getElementById('song');
-function playSound(){
-    mp3.play();
-}
-window.onload = function (){
-    printSentence('diii', h1Text, 100);
-    playSound();
-};
+// var h1Text = "SHALL WE PLAY A GAME?";
+// function printSentence(id, sentence, speed){
+//     var index=0;
+//     var intObject= setInterval(function() {
+//     document.getElementById(id).innerHTML+=sentence[index];
+//     index++;
+//     if(index==sentence.length){
+//     clearInterval(intObject);
+//     }
+//     }, speed);
+// }
+// var mp3 = document.getElementById('song');
+// function playSound(){
+//     mp3.play();
+// }
+// window.onload = function (){
+//     printSentence('diii', h1Text, 100);
+//     playSound();
+// };
