a49471886df3899ffe2cb13efe71af8e27a313c4
Denis Molloy <denis.molloy@gmail.com>
2018-03-02 23:22:15 -0800

    audio function done

--- javascript/app.js
+++ javascript/app.js
@@ -1,29 +1,58 @@
-// var h1Text = "SHALL WE PLAY A GAME?";
-// function printSentence(id, sentence, speed){
-//     var index=0;
-//     var intObject= setInterval(function() {
-//     document.getElementById(id).innerHTML+=sentence[index];
-//     index++;
-//     if(index==sentence.length){
-//     clearInterval(intObject);
-//     }
-//     }, speed);
-// }
-// var mp3 = document.getElementById('song');
-// function playSound(){
-//     mp3.play();
-// }
-// window.onload = function (){
-//     printSentence('diii', h1Text, 100);
-//     playSound();
-// };
+$(document).ready(() => {
+  var shallWe = "SHALL WE PLAY A GAME?";
+  var rules = "Victory condition: 5 wins.";
+  function printString(id, sentence, speed) {
+    var index = 0;
+    var intObject = setInterval(() => {
+      document.getElementById(id).innerHTML += sentence[index];
+      index++;
+      if (index == sentence.length) {
+        clearInterval(intObject);
+      }
+    }, speed);
+  }
 
-$(document).ready(()=>{
+  function playSound(cue) {
+    var sounds = {
+      greet: "assets/play.mp3",
+      spangled: "assets/spangled.mp3",
+      round: "assets/round.wav",
+      win: "assets/win.wav",
+      loss: "assets/loss.wav"
+    };
 
-    $("#begin").on("click", ()=>{
-        $("#message-div").hide()
-        $("#playground").css("display", "block");  
-    })
-    
+    switch (cue) {
+      case "greet":
+        $("#song").attr("src", sounds.greet);
+        break;
+      case "spangled":
+        $("#song").attr("src", sounds.spangled);
+        break;
+        case "round":
+        $("#song").attr("src", sounds.round);
+        break;
+        case "win":
+        $("#song").attr("src", sounds.win);
+        break;
+        case "lost":
+        $("#song").attr("src", sounds.loss)
+    }
 
-})
+    $("#song").trigger("play");
+  }
+  printString("message", shallWe, 100);
+  playSound("greet");
+
+  $("#begin").on("click", () => {
+    $(".fade").fadeOut("slow");
+    $("#begin").toggleClass("weapon");
+    setTimeout(() => {
+      printString("message-rule", rules, 30);
+    }, 2000);
+    setTimeout(() => {
+      $("#message-div").hide();
+      $("#playground").css("display", "block");
+      playSound("spangled")
+    }, 4000);
+  });
+});
