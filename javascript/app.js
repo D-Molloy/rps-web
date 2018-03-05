13ee79665201447f97754656e3f156d4cd19e972
Denis Molloy <denis.molloy@gmail.com>
2018-03-04 22:24:14 -0800

    working on logic

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   var shallWe = "SHALL WE PLAY A GAME?";
-  var rules = "Victory condition: 5 wins.";
+  var rules = "Victory condition: 3 wins.";
   function printString(id, sentence, speed) {
     var index = 0;
     var intObject = setInterval(() => {
@@ -28,20 +28,59 @@ $(document).ready(() => {
       case "spangled":
         $("#song").attr("src", sounds.spangled);
         break;
-        case "round":
+      case "round":
         $("#song").attr("src", sounds.round);
         break;
-        case "win":
+      case "win":
         $("#song").attr("src", sounds.win);
         break;
-        case "lost":
-        $("#song").attr("src", sounds.loss)
+      case "lost":
+        $("#song").attr("src", sounds.loss);
     }
 
     $("#song").trigger("play");
   }
-  printString("message", shallWe, 100);
-  playSound("greet");
+
+  let wins = 0;
+  let losses = 0;
+  let draws = 0;
+  let userChoice;
+  let enemyChoice;
+  const choices = ["ROCK", "PAPER", "SCISSORS"];
+  let random = Math.floor(Math.random() * 3);
+
+  const playRound = (userAtt, enemyAtt) => {
+    $("#action-enemy-choice").text(enemyChoice);
+    playSound("round");
+
+    if (userAtt === "ROCK" && enemyAtt === "SCISSORS") {
+      wins++;
+    }
+
+    if (userAtt === "ROCK" && enemyAtt === "PAPER") {
+      losses++;
+    }
+
+    if (userAtt === "SCISSORS" && enemyAtt === "ROCK") {
+      losses++;
+    }
+
+    if (userAtt === "SCISSORS" && enemyAtt === "PAPER") {
+      wins++;
+    }
+
+    if (userAtt === "PAPER" && enemyAtt === "ROCK") {
+      wins++;
+    }
+
+    if (userAtt === "PAPER"&& enemyAtt === "SCISSORS") {
+      losses++;
+    }
+
+    if (userAtt === enemyAtt) {
+      ties++;
+    }
+  }; //end playRound
 
   $("#begin").on("click", () => {
     $(".fade").fadeOut("slow");
@@ -52,7 +91,20 @@ $(document).ready(() => {
     setTimeout(() => {
       $("#message-div").hide();
       $("#playground").css("display", "block");
-      playSound("spangled")
+      playSound("spangled");
     }, 4000);
   });
+
+  $(".weapon").on("click", function() {
+    userChoice = $(this)
+      .text()
+      .trim();
+    playSound("round");
+    $("#action-user-choice").text(userChoice);
+    enemyChoice = choices[random];
+    setTimeout(() => playRound(userChoice, enemyChoice), 1000);
+  });
+
+  printString("message", shallWe, 100);
+  playSound("greet");
 });
